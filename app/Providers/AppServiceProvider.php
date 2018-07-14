8a3ba301ec0770d53d99f8e24e7688b4511e02d6
wangpeng <wp782452336@163.com>
2018-07-14 20:52:41 +0800

    添加注册验证码

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Blade;
+// use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::component('components.alert', 'alert');
+        // Blade::component('components.alert', 'alert');
+        Carbon::setLocale('zh');
         //Blade::withoutDoubleEncoding();
     }
 
