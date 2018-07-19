219924e7ed6b10e9a28838ead846e1de6f8f1b05
wangpeng <wp782452336@163.com>
2018-07-19 11:00:16 +0800

    创建模型及迁移

---
+++ app/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'categories';
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+}
